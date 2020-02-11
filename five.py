838dc8fb805394bcec67e9fc934bbd60337fd6cd
Joel M. Gottlieb <joel.gottlieb@gmail.com>
2020-02-10 19:50:33 -0500

    Adding additional Dijkstra problems

---
+++ five.py
@@ -0,0 +1,20 @@
+from dijkstra_solver import dijkstra_solver
+import networkx as nx
+from dimod import BinaryQuadraticModel
+import dimod
+import dwave.inspector
+from dwave.system import DWaveSampler, EmbeddingComposite
+
+
+G = nx.Graph()
+G.add_weighted_edges_from([('S', 'a', 5.0), ('a', 'G', 6.0), ('G', 'c', 3.0), ('c', 'd', 4.0), ('d', 'S', 5.0)])
+lagrange = 20
+chainstrength = 20
+numruns = 1000
+
+model = dijkstra_solver(G, 'S', 'G', lagrange)
+Q = model.to_qubo()
+bqm = BinaryQuadraticModel.from_qubo(Q[0], offset=Q[1])
+sampler = EmbeddingComposite(DWaveSampler())
+sampleset = sampler.sample(bqm, chain_strength=chainstrength, num_reads=numruns)
+dwave.inspector.show(bqm, sampleset, sampler)
